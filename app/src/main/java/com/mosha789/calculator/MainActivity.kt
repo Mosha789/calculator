7b1baa0f18cc18411edd6d3301ba3b354f3e1a03
Mosha789 <86707526+Mosha789@users.noreply.github.com>
2025-08-11 12:27:10 +0600

    Add MainActivity with basic calculator layout

---
+++ app/src/main/java/com/mosha789/calculator/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mosha789.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalculatorScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculatorScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Display screen will be added here
+        CalculatorDisplay()
+        // Button grid will be added here
+        CalculatorKeypad()
+    }
+}
